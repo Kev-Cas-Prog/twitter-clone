1e50d6c3960e44c3b781641e1aba16cbf7463b33
IsaacCastelo <isaacpxl8@gmail.com>
2024-07-10 13:10:01 -0700

    3- Auth handled and routes protected in client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,64 @@
 //Routes
-import {Route, Routes} from 'react-router-dom'
+import {Route, Routes, Navigate} from 'react-router-dom';
 //Pages
-import HomePage from './pages/home/HomePage'
-import LoginPage from './pages/auth/login/LoginPage'
-import SignUpPage from './pages/auth/signup/SignUpPage'
-import NotificationPage from './pages/notification/NotificationPage'
-import ProfilePage from './pages/profile/ProfilePage'
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/auth/login/LoginPage';
+import SignUpPage from './pages/auth/signup/SignUpPage';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
 //Components
-import Sidebar from './components/common/Sidebar'
-import RightPanel from './components/common/RightPanel'
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
+
+import { Toaster } from 'react-hot-toast';
+import {useQuery} from '@tanstack/react-query';
+import LoadingSpinner from './components/common/LoadingSpinner';
 
 
 function App() {
+  const {data:authUser, isLoading} = useQuery({
+    //we use queryKey to give a unique name to our query and refer to it later
+    queryKey: ['authUser'],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+        if(data.error) return null;
+        if(!res.ok){
+          throw new Error(data.error || "Something went wrong")
+        }
+        console.log("authUser is here: ", data);
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry: false, // tries to enter page one time
+  });
+
+  if(isLoading) {
+    return (
+      <div className='h-screen flex justify-center items-center'>
+        <LoadingSpinner size='lg'/>
+      </div>
+    )
+  }
+
   return (
     <div className='flex max-w-6xl mx-auto'>
       {/* Common Component, bc it's not wrapped with Routes */}
-      <Sidebar />
+      {authUser && <Sidebar />} {/* appears if authUser is not null */}
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/notifications' element={<NotificationPage />}/>
-        <Route path='/profile/:username' element={<ProfilePage />}/>
+        <Route path='/' element={authUser ?  <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to="/login" />} />
+        <Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
-      <RightPanel />
+      {authUser && <RightPanel />} {/* appears if authUser is not null */}
+      <Toaster/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
